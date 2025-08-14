a95b4e8c41e366144550407050c3ad750a5edc14
Anatoliy Kotliar <blizzkon@gmail.com>
2025-08-14 05:13:00 +0300

    feat: add all hero characters data

--- src/modules/characters.js
+++ src/modules/characters.js
@@ -1,9 +1,11 @@
 import {Character} from './characterComponent'
 import {_SCHOOL_GIRL} from './_CHAR_DATA'
+import {_CHARS_DATA} from './_CHAR_DATA'
 export const charactersUi = () => {
   const characterBlock = document.querySelector('.character')
   const characterBlockOne = document.querySelector('.characterOne')
   const characterThree = document.querySelector('.characterThree')
+  const satyrOne = document.querySelector('.satyrOne')
   const schoolGirl = new Character({
     skin: _SCHOOL_GIRL.defoult,
     frameWidth: 128,
@@ -22,8 +24,14 @@ export const charactersUi = () => {
     frameHeight: 128,
     container: characterThree,
   })
-  schoolGirl.changeSkin(_SCHOOL_GIRL.skinThree)
-  schoolGirl3.changeSkin(_SCHOOL_GIRL.skinTwo)
+  const satyr = new Character({
+    skin: _CHARS_DATA[3].skinThree,
+    frameWidth: 128,
+    frameHeight: 128,
+    container: satyrOne,
+  })
+  // schoolGirl.changeSkin(_SCHOOL_GIRL.skinThree)
+  // schoolGirl3.changeSkin(_SCHOOL_GIRL.skinTwo)
   // schoolGirl.attack()
   // schoolGirl.walk()
   // schoolGirl.protect()
@@ -31,4 +39,5 @@ export const charactersUi = () => {
   // schoolGirl2.attack()
   // schoolGirl.idle()
   // schoolGirl3.attack()
+  // satyr.attack()
 }
