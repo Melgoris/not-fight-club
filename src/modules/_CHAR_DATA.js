638d52fd32f28c0367db8d56421d0ab3a786c2e8
Anatoliy Kotliar <blizzkon@gmail.com>
2025-08-14 06:26:11 +0300

    fix: refactor heros data

--- src/modules/_CHAR_DATA.js
+++ src/modules/_CHAR_DATA.js
@@ -83,291 +83,210 @@ export const _SCHOOL_GIRL = {
     },
   },
 }
-export const _CHARS_NAMES = ['School girl', 'Satyr']
-export const _CHARS_DATA = [
-  {
-    defoult: {
-      attack: {
-        sprite: '/img/characters/schoolGirl/defoultSkin/Attack.png',
-        totalFrames: 8,
-        fps: 8,
-      },
-      dialogue: {
-        sprite: '/img/characters/schoolGirl/defoultSkin/Dialogue.png',
-        totalFrames: 11,
-        fps: 8,
-      },
-      idle: {
-        sprite: '/img/characters/schoolGirl/defoultSkin/Idle.png',
-        totalFrames: 9,
-        fps: 8,
-      },
-      walk: {
-        sprite: '/img/characters/schoolGirl/defoultSkin/Walk.png',
-        totalFrames: 12,
-        fps: 17,
-      },
-      protect: {
-        sprite: '/img/characters/schoolGirl/defoultSkin/Protection.png',
-        totalFrames: 4,
-        fps: 8,
-      },
+export const _SATYR = {
+  defoult: {
+    attack: {
+      sprite: '/img/characters/satyr/defoultSkin/Attack.png',
+      totalFrames: 4,
+      fps: 8,
     },
-    skinTwo: {
-      attack: {
-        sprite: '/img/characters/schoolGirl/skinTwo/Attack.png',
-        totalFrames: 9,
-        fps: 8,
-        endFrame: 6,
-      },
-      dialogue: {
-        sprite: '/img/characters/schoolGirl/skinTwo/Dialogue.png',
-        totalFrames: 6,
-        fps: 8,
-      },
-      idle: {
-        sprite: '/img/characters/schoolGirl/skinTwo/Idle.png',
-        totalFrames: 7,
-        fps: 7,
-      },
-      walk: {
-        sprite: '/img/characters/schoolGirl/skinTwo/Walk.png',
-        totalFrames: 12,
-        fps: 17,
-      },
-      protect: {
-        sprite: '/img/characters/schoolGirl/skinTwo/Protection.png',
-        totalFrames: 2,
-        fps: 8,
-      },
+    dead: {
+      sprite: '/img/characters/satyr/defoultSkin/Dead.png',
+      totalFrames: 4,
+      fps: 5,
     },
-    skinThree: {
-      attack: {
-        sprite: '/img/characters/schoolGirl/skinThree/Attack.png',
-        totalFrames: 6,
-        fps: 8,
-        endFrame: 1,
-      },
-      dialogue: {
-        sprite: '/img/characters/schoolGirl/skinThree/Dialogue.png',
-        totalFrames: 5,
-        fps: 8,
-      },
-      idle: {
-        sprite: '/img/characters/schoolGirl/skinThree/Idle.png',
-        totalFrames: 6,
-        fps: 7,
-      },
-      walk: {
-        sprite: '/img/characters/schoolGirl/skinThree/Walk.png',
-        totalFrames: 12,
-        fps: 17,
-      },
-      protect: {
-        sprite: '/img/characters/schoolGirl/skinThree/Protection.png',
-        totalFrames: 3,
-        fps: 8,
-      },
+    idle: {
+      sprite: '/img/characters/satyr/defoultSkin/Idle.png',
+      totalFrames: 7,
+      fps: 8,
+    },
+    walk: {
+      sprite: '/img/characters/satyr/defoultSkin/Walk.png',
+      totalFrames: 12,
+      fps: 17,
+    },
+    protect: {
+      sprite: '/img/characters/satyr/defoultSkin/Protection.png',
+      totalFrames: 4,
+      fps: 8,
     },
   },
-  {
-    defoult: {
-      attack: {
-        sprite: '/img/characters/satyr/defoultSkin/Attack.png',
-        totalFrames: 4,
-        fps: 8,
-      },
-      dead: {
-        sprite: '/img/characters/satyr/defoultSkin/Dead.png',
-        totalFrames: 4,
-        fps: 5,
-      },
-      idle: {
-        sprite: '/img/characters/satyr/defoultSkin/Idle.png',
-        totalFrames: 7,
-        fps: 8,
-      },
-      walk: {
-        sprite: '/img/characters/satyr/defoultSkin/Walk.png',
-        totalFrames: 12,
-        fps: 17,
-      },
-      protect: {
-        sprite: '/img/characters/satyr/defoultSkin/Protection.png',
-        totalFrames: 4,
-        fps: 8,
-      },
+  skinTwo: {
+    attack: {
+      sprite: '/img/characters/satyr/skinTwo/Attack.png',
+      totalFrames: 8,
+      fps: 8,
+      endFrame: 6,
     },
-    skinTwo: {
-      attack: {
-        sprite: '/img/characters/satyr/skinTwo/Attack.png',
-        totalFrames: 8,
-        fps: 8,
-        endFrame: 6,
-      },
-      dead: {
-        sprite: '/img/characters/satyr/skinTwo/Dead.png',
-        totalFrames: 4,
-        fps: 5,
-      },
-      idle: {
-        sprite: '/img/characters/satyr/skinTwo/Idle_fixtwo.png',
-        totalFrames: 7,
-        fps: 7,
-      },
-      walk: {
-        sprite: '/img/characters/satyr/skinTwo/Walk_fixed.png',
-        totalFrames: 12,
-        fps: 15,
-      },
-      protect: {
-        sprite: '/img/characters/satyr/skinTwo/Protection.png',
-        totalFrames: 4,
-        fps: 5,
-      },
+    dead: {
+      sprite: '/img/characters/satyr/skinTwo/Dead.png',
+      totalFrames: 4,
+      fps: 5,
+    },
+    idle: {
+      sprite: '/img/characters/satyr/skinTwo/Idle_fixtwo.png',
+      totalFrames: 7,
+      fps: 7,
+    },
+    walk: {
+      sprite: '/img/characters/satyr/skinTwo/Walk_fixed.png',
+      totalFrames: 12,
+      fps: 15,
+    },
+    protect: {
+      sprite: '/img/characters/satyr/skinTwo/Protection.png',
+      totalFrames: 4,
+      fps: 5,
     },
   },
-  {
-    defoult: {
-      attack: {
-        sprite: '/img/characters/vampire/defoultSkin/Attack_1.png',
-        totalFrames: 5,
-        fps: 8,
-      },
-      dead: {
-        sprite: '/img/characters/vampire/defoultSkin/Dead.png',
-        totalFrames: 10,
-        fps: 14,
-      },
-      idle: {
-        sprite: '/img/characters/vampire/defoultSkin/Idle.png',
-        totalFrames: 5,
-        fps: 7,
-      },
-      walk: {
-        sprite: '/img/characters/vampire/defoultSkin/Run.png',
-        totalFrames: 6,
-        fps: 7,
-      },
-      protect: {
-        sprite: '/img/characters/vampire/defoultSkin/Hurt.png',
-        totalFrames: 2,
-        fps: 5,
-      },
+}
+export const _VAMPIRE = {
+  defoult: {
+    attack: {
+      sprite: '/img/characters/vampire/defoultSkin/Attack_1.png',
+      totalFrames: 5,
+      fps: 8,
     },
-    skinTwo: {
-      attack: {
-        sprite: '/img/characters/vampire/skinTwo/Attack_1.png',
-        totalFrames: 6,
-        fps: 8,
-        endFrame: 4,
-      },
-      dead: {
-        sprite: '/img/characters/vampire/skinTwo/Dead.png',
-        totalFrames: 8,
-        fps: 5,
-      },
-      idle: {
-        sprite: '/img/characters/vampire/skinTwo/Idle.png',
-        totalFrames: 5,
-        fps: 7,
-      },
-      walk: {
-        sprite: '/img/characters/vampire/skinTwo/Walk.png',
-        totalFrames: 6,
-        fps: 9,
-      },
-      protect: {
-        sprite: '/img/characters/vampire/skinTwo/Hurt.png',
-        totalFrames: 2,
-        fps: 5,
-      },
+    dead: {
+      sprite: '/img/characters/vampire/defoultSkin/Dead.png',
+      totalFrames: 10,
+      fps: 14,
+    },
+    idle: {
+      sprite: '/img/characters/vampire/defoultSkin/Idle.png',
+      totalFrames: 5,
+      fps: 7,
+    },
+    walk: {
+      sprite: '/img/characters/vampire/defoultSkin/Run.png',
+      totalFrames: 6,
+      fps: 7,
+    },
+    protect: {
+      sprite: '/img/characters/vampire/defoultSkin/Hurt.png',
+      totalFrames: 2,
+      fps: 5,
     },
   },
-  {
-    defoult: {
-      attack: {
-        sprite: '/img/characters/gorgon/defoultSkin/Attack_3.png',
-        totalFrames: 10,
-        fps: 8,
-        endFrame: 4,
-      },
-      dead: {
-        sprite: '/img/characters/gorgon/defoultSkin/Dead.png',
-        totalFrames: 3,
-        fps: 5,
-      },
-      idle: {
-        sprite: '/img/characters/gorgon/defoultSkin/Idle.png',
-        totalFrames: 7,
-        fps: 8,
-      },
-      walk: {
-        sprite: '/img/characters/gorgon/defoultSkin/Run.png',
-        totalFrames: 7,
-        fps: 8,
-      },
-      protect: {
-        sprite: '/img/characters/gorgon/defoultSkin/Hurt.png',
-        totalFrames: 3,
-        fps: 5,
-      },
+  skinTwo: {
+    attack: {
+      sprite: '/img/characters/vampire/skinTwo/Attack_1.png',
+      totalFrames: 6,
+      fps: 8,
+      endFrame: 4,
     },
-    skinTwo: {
-      attack: {
-        sprite: '/img/characters/gorgon/skinTwo/Attack_3.png',
-        totalFrames: 10,
-        fps: 8,
-        endFrame: 4,
-      },
-      dead: {
-        sprite: '/img/characters/gorgon/skinTwo/Dead.png',
-        totalFrames: 3,
-        fps: 5,
-      },
-      idle: {
-        sprite: '/img/characters/gorgon/skinTwo/Idle.png',
-        totalFrames: 7,
-        fps: 8,
-      },
-      walk: {
-        sprite: '/img/characters/gorgon/skinTwo/Run.png',
-        totalFrames: 7,
-        fps: 8,
-      },
-      protect: {
-        sprite: '/img/characters/gorgon/skinTwo/Hurt.png',
-        totalFrames: 3,
-        fps: 5,
-      },
+    dead: {
+      sprite: '/img/characters/vampire/skinTwo/Dead.png',
+      totalFrames: 8,
+      fps: 5,
     },
-    skinThree: {
-      attack: {
-        sprite: '/img/characters/gorgon/skinThree/Attack_2.png',
-        totalFrames: 10,
-        fps: 8,
-        endFrame: 4,
-      },
-      dead: {
-        sprite: '/img/characters/gorgon/skinThree/Dead.png',
-        totalFrames: 3,
-        fps: 5,
-      },
-      idle: {
-        sprite: '/img/characters/gorgon/skinThree/Idle.png',
-        totalFrames: 7,
-        fps: 8,
-      },
-      walk: {
-        sprite: '/img/characters/gorgon/skinThree/Run.png',
-        totalFrames: 7,
-        fps: 8,
-      },
-      protect: {
-        sprite: '/img/characters/gorgon/skinThree/Hurt.png',
-        totalFrames: 3,
-        fps: 5,
-      },
+    idle: {
+      sprite: '/img/characters/vampire/skinTwo/Idle.png',
+      totalFrames: 5,
+      fps: 7,
+    },
+    walk: {
+      sprite: '/img/characters/vampire/skinTwo/Walk.png',
+      totalFrames: 6,
+      fps: 9,
+    },
+    protect: {
+      sprite: '/img/characters/vampire/skinTwo/Hurt.png',
+      totalFrames: 2,
+      fps: 5,
     },
   },
+}
+export const _GORGON = {
+  defoult: {
+    attack: {
+      sprite: '/img/characters/gorgon/defoultSkin/Attack_3.png',
+      totalFrames: 10,
+      fps: 8,
+      endFrame: 4,
+    },
+    dead: {
+      sprite: '/img/characters/gorgon/defoultSkin/Dead.png',
+      totalFrames: 3,
+      fps: 5,
+    },
+    idle: {
+      sprite: '/img/characters/gorgon/defoultSkin/Idle.png',
+      totalFrames: 7,
+      fps: 8,
+    },
+    walk: {
+      sprite: '/img/characters/gorgon/defoultSkin/Run.png',
+      totalFrames: 7,
+      fps: 8,
+    },
+    protect: {
+      sprite: '/img/characters/gorgon/defoultSkin/Hurt.png',
+      totalFrames: 3,
+      fps: 5,
+    },
+  },
+  skinTwo: {
+    attack: {
+      sprite: '/img/characters/gorgon/skinTwo/Attack_3.png',
+      totalFrames: 10,
+      fps: 8,
+      endFrame: 4,
+    },
+    dead: {
+      sprite: '/img/characters/gorgon/skinTwo/Dead.png',
+      totalFrames: 3,
+      fps: 5,
+    },
+    idle: {
+      sprite: '/img/characters/gorgon/skinTwo/Idle.png',
+      totalFrames: 7,
+      fps: 8,
+    },
+    walk: {
+      sprite: '/img/characters/gorgon/skinTwo/Run.png',
+      totalFrames: 7,
+      fps: 8,
+    },
+    protect: {
+      sprite: '/img/characters/gorgon/skinTwo/Hurt.png',
+      totalFrames: 3,
+      fps: 5,
+    },
+  },
+  skinThree: {
+    attack: {
+      sprite: '/img/characters/gorgon/skinThree/Attack_2.png',
+      totalFrames: 10,
+      fps: 8,
+      endFrame: 4,
+    },
+    dead: {
+      sprite: '/img/characters/gorgon/skinThree/Dead.png',
+      totalFrames: 3,
+      fps: 5,
+    },
+    idle: {
+      sprite: '/img/characters/gorgon/skinThree/Idle.png',
+      totalFrames: 7,
+      fps: 8,
+    },
+    walk: {
+      sprite: '/img/characters/gorgon/skinThree/Run.png',
+      totalFrames: 7,
+      fps: 8,
+    },
+    protect: {
+      sprite: '/img/characters/gorgon/skinThree/Hurt.png',
+      totalFrames: 3,
+      fps: 5,
+    },
+  },
+}
+
+export const _CHARS = [
+  {id: 'school_girl', name: 'School girl', skins: _SCHOOL_GIRL},
+  {id: 'satyr', name: 'Satyr', skins: _SATYR},
+  {id: 'vampire', name: 'Vampire', skins: _VAMPIRE},
+  {id: 'gorgon', name: 'Gorgon', skins: _GORGON},
 ]
--- src/modules/characters.js
+++ src/modules/characters.js
@@ -1,43 +1,16 @@
 import {Character} from './characterComponent'
-import {_SCHOOL_GIRL} from './_CHAR_DATA'
-import {_CHARS_DATA} from './_CHAR_DATA'
+
+import {_CHARS} from './_CHAR_DATA'
 export const charactersUi = () => {
   const characterBlock = document.querySelector('.character')
-  const characterBlockOne = document.querySelector('.characterOne')
-  const characterThree = document.querySelector('.characterThree')
-  const satyrOne = document.querySelector('.satyrOne')
   const schoolGirl = new Character({
-    skin: _SCHOOL_GIRL.defoult,
+    skin: _CHARS[0].skins.defoult,
     frameWidth: 128,
     frameHeight: 128,
     container: characterBlock,
   })
-  const schoolGirl2 = new Character({
-    skin: _SCHOOL_GIRL.defoult,
-    frameWidth: 128,
-    frameHeight: 128,
-    container: characterBlockOne,
-  })
-  const schoolGirl3 = new Character({
-    skin: _SCHOOL_GIRL.defoult,
-    frameWidth: 128,
-    frameHeight: 128,
-    container: characterThree,
-  })
-  const satyr = new Character({
-    skin: _CHARS_DATA[3].skinThree,
-    frameWidth: 128,
-    frameHeight: 128,
-    container: satyrOne,
-  })
-  // schoolGirl.changeSkin(_SCHOOL_GIRL.skinThree)
-  // schoolGirl3.changeSkin(_SCHOOL_GIRL.skinTwo)
-  // schoolGirl.attack()
-  // schoolGirl.walk()
-  // schoolGirl.protect()
-  // schoolGirl.attack()
-  // schoolGirl2.attack()
-  // schoolGirl.idle()
+
+  schoolGirl.idle()
   // schoolGirl3.attack()
   // satyr.attack()
 }
